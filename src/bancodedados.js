8d7a1050d8b93bef8f152f1949237d1e3d9bcae7
Joao Filho <joaofilhomil@gmail.com>
2023-07-25 16:07:37 -0300

    primeiro commit

---
+++ src/bancodedados.js
@@ -0,0 +1,24 @@
+const bancoDeDados = {
+    identificadorInstrutor: 3,
+    instrutores: [
+        {
+            id: 1,
+            nome: 'João Filho',
+            email: 'joao@gmail.com',
+            status: true
+        },
+        {
+            id: 2,
+            nome: 'Maria',
+            email: 'maria@gmail.com',
+            status: true
+        },
+        {
+            id: 3,
+            nome: 'Dani',
+            email: 'dani@gmail.com',
+            status: true
+        }
+    ]
+}
+module.exports = bancoDeDados;
